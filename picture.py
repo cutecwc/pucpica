8a5b05449a2255c7bd350a962ff0e559bf0263ed
cutecwc <cwcsocute@gmail.com>
2023-06-22 18:47:57 +0800

    cmt

---
+++ picture.py
@@ -0,0 +1,34 @@
+# 如何使用这个文件
+# 设置虚拟环境：
+# sudo pacman -S python-virtualenv
+# python -m  virtualenv myenv
+# 进入虚拟环境
+# source myenv/bin/activate
+# 退出虚拟环境
+# deactivate
+
+# 需要使用到的包：
+# pip install pillow-avif-plugin
+# pip install Pillow
+
+TARGET_PATH = 'y23m6'
+
+from PIL import Image
+import pillow_avif
+
+import os
+def func_FilePaser_V2(filepath):
+    # TODO: edit to open md? files from file system.
+    for filepathe, dirs, fs in os.walk(filepath):
+        for f in fs:
+            # print(os.path.join(filepathe, f))
+            # print(f)
+
+            filepath_temp = os.path.join(filepathe, f)
+
+            imgss = Image.open(filepath_temp)
+            imgss.save(filepath_temp + '.AVIF','AVIF')
+            os.remove(filepath_temp)
+            ...
+
+func_FilePaser_V2(TARGET_PATH)
